6be4f2b4c2aa30a586c0a936fead191f18feed80
Adnan-D-Luffy <abdullahal467bp@gmail.com>
2025-07-31 03:19:28 -0400

    bools

---
+++ bool.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main () {
+    // Bool contains true or false
+    bool a = true;
+    bool b = false ;
+
+    printf("%d \n" ,b);
+    if (a) {
+        printf("You are a good boy");
+    }
+    else {
+        printf("You are not a good boy .");
+    }
+    return 0;
+}
